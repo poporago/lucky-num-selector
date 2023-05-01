a8848e6535d3af206bc26c93fb4a00abc19797ab
poporago <dideoeo555@gmail.com>
2023-05-01 15:30:10 +0900

    "fix : Replace i to j, Adjust End num 20 to i6"

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,11 @@
-print("it works")
+for j in range(1,16+1):
+    if j%15==0:
+        print('fizzbuzz')
+    elif j%3==0:
+        print('fizz')
+    elif j%5==0:
+        print('buzz')
+    else:
+        print(f'{j}')
 
 
